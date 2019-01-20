e8d9521e5bc5ae1da1738405b59477aafd40d7e1
Sahil Gandhi <sahilmgandhi@gmail.com>
2019-01-19 18:11:17 -0800

    Add modal for error code image

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # prequel-error-codes
 Change your website error codes to StarWars®™ prequel memes
+
+## Supported Error Codes:
+
+1. 308 - Permanent Redirect
+2. 400 - Bad Request
+3. 403 - Forbidden
+4. 404 - Not Found
+5. 417 - Expectation Failed
+6. 500 - Internal Server Error
+7. 503 - Service Unavailable
+
--- content.js
+++ content.js
@@ -1,4 +1,61 @@
-var images = document.getElementsByTagName('img');
-for (var i = 0, l = images.length; i < l; i++) {
-  images[i].src = 'http://placekitten.com/' + images[i].width + '/' + images[i].height;
+// Sahil Gandhi
+
+
+// var images = document.getElementsByTagName('img');
+// for (var i = 0, l = images.length; i < l; i++) {
+//   images[i].src = 'http://placekitten.com/' + images[i].width + '/' + images[i].height;
+// }
+
+var url = window.location.href;
+
+var xhr = new XMLHttpRequest();
+xhr.open('GET', url, true);
+xhr.send();
+
+xhr.onreadystatechange = processRequest;
+
+var wrapper = document.createElement("div");
+
+var innerHTML = `<div class="modal">
+<div class="modal-content">
+    <span class="close-button">×</span>`;
+
+// <
+// h1 > Hello, I am a modal! < /h1> <
+
+var modal;
+var closeButton;
+
+function processRequest(e) {
+  if (xhr.readyState == 4) {
+
+    let currStatus = xhr.status;
+
+    console.log("The status is: " + currStatus);
+
+    switch (currStatus) {
+      case 404:
+        innerHTML += `<h1>404 Error</h1>`;
+        innerHTML += `<img src="https://github.com/sahilmgandhi/prequel-error-codes/blob/master/rawErrorCodes.png?raw=true" alt="404 Image" height="200" width="200">`;
+        break;
+
+      default:
+        return;
+    }
+
+    innerHTML += `</div> </div>`;
+    wrapper.innerHTML = innerHTML;
+
+    // Show the modal
+    document.body.appendChild(wrapper);
+    modal = document.querySelector(".modal");
+    modal.classList.toggle("show-modal");
+
+    closeButton = document.querySelector(".close-button");
+    closeButton.addEventListener("click", toggleModal);
+  }
+}
+
+function toggleModal() {
+  modal.classList.toggle("show-modal");
 }
